bd9581c16bf584069c5c7e08dd7e192d2a4ef9ae
jeanettepong <jeanettepong@gmail.com>
2018-02-06 15:06:51 -0500

    website

--- Web/website/CSL/urls.py
+++ Web/website/CSL/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index.html', views.index),
+    path('HVAC.html', views.HVAC),
     path('about.html', views.about),
+    path('service.html', views.service),
+    path('tbd.html', views.tbd),
 ]
--- Web/website/CSL/views.py
+++ Web/website/CSL/views.py
@@ -5,5 +5,14 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'CSL/template/index.html')
     
+def HVAC(request):
+    return render(request, 'CSL/template/HVAC.html')
+    
 def about(request):
     return render(request, 'CSL/template/about.html')
+    
+def service(request):
+    return render(request, 'CSL/template/service.html')
+    
+def tbd(request):
+    return render(request, 'CSL/template/tbd.html')
