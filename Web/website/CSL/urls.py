92cd54085031ea5d0862ab9419dddf1937587fac
AceChenXu <ace.chenxu@gmail.com>
2018-02-08 17:29:29 -0500

    update mechanical overview page

--- Web/website/CSL/urls.py
+++ Web/website/CSL/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('about.html', views.about),
     path('service.html', views.service),
     path('tbd.html', views.tbd),
+    path('mech-sys-overview.html', views.mech_sys_overview),
+    path('mech-sys-nv.html', views.mech_sys_nv),
+    path('mech-sys-geo.html', views.mech_sys_geo),
+    path('mech-sys-ufds.html', views.mech_sys_ufds),
+    path('mech-sys-tcers.html', views.mech_sys_tcers),
+    path('dynamic-data-mech.html', views.dynamic_data_mech),
 ]
--- Web/website/CSL/views.py
+++ Web/website/CSL/views.py
@@ -4,15 +4,33 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'CSL/template/index.html')
-    
+
 def HVAC(request):
     return render(request, 'CSL/template/HVAC.html')
-    
+
 def about(request):
     return render(request, 'CSL/template/about.html')
-    
+
 def service(request):
     return render(request, 'CSL/template/service.html')
-    
+
 def tbd(request):
     return render(request, 'CSL/template/tbd.html')
+
+def mech_sys_overview(request):
+    return render(request, 'CSL/template/mech-sys-overview.html')
+
+def mech_sys_nv(request):
+    return render(request, 'CSL/template/mech-sys-nv.html')
+
+def mech_sys_geo(request):
+    return render(request, 'CSL/template/mech-sys-geo.html')
+
+def mech_sys_ufds(request):
+    return render(request, 'CSL/template/mech-sys-ufds.html')
+
+def mech_sys_tcers(request):
+    return render(request, 'CSL/template/mech-sys-tcers.html')
+
+def dynamic_data_mech(request):
+    return render(request, 'CSL/template/dynamic-data-mech.html')
