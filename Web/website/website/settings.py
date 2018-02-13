2e68179f3841e3a8d0f52dcf4ab75c9ad1afa655
AceChenXu <ace.chenxu@gmail.com>
2018-02-13 17:40:58 -0500

    change views.py

--- Web/website/website/settings.py
+++ Web/website/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '46d0j0a&u$12j44(ke=mj7um6nm=v_s4mf#iup+e=_u8ye%#)f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.228.12.253']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CSL.apps.CslConfig',
+    'CSL',
 ]
 
 MIDDLEWARE = [
